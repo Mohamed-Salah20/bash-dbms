8f36ece275129646cb5d7397d24e3fe49f6c1e10
Mohamed-Salah20 <mohamed.salah.ms2000@gmail.com>
2024-03-08 18:13:45 +0200

    first skeleton of the project

--- dbms.sh
+++ dbms.sh
@@ -1,2 +1,52 @@
 #!/bin/bash
 
+# Display main menu
+main_menu(){
+    echo "Main Menu:"
+    echo "1. create database"
+    echo "2. list databases"
+    echo "3. connect to databases"
+    echo "4. drop database"
+    echo "5. exit"
+}
+
+# Display Database menu
+database_menu(){
+
+}
+
+#####database_functions#####
+create_database(){
+
+}
+
+list_databases(){
+
+}
+
+drop_database(){
+
+}
+
+connect_database(){
+
+}
+########end of database_functions######
+
+####database_table_functions#####
+
+create_database_table(){
+
+}
+
+list_database_tables(){
+
+}
+
+drop_database_table(){
+
+}
+
+#######end of database_table_functions##########
+
+#################starting of the script################
