b0b498e01451ad7167f00f08c729937019bbfbe0
Mohamed-Salah20 <mohamed.salah.ms2000@gmail.com>
2024-03-08 19:09:17 +0200

    first draft of create database directory

--- dbms.sh
+++ dbms.sh
@@ -1,52 +1,90 @@
 #!/bin/bash
 
+
+# create db folder for the first script run in a certin path
+if [ ! -d db ]; then
+mkdir db
+fi
+
+
 # Display main menu
-main_menu(){
+main_menu_display(){
+    echo -e "\n"
+    echo "***********"
     echo "Main Menu:"
     echo "1. create database"
     echo "2. list databases"
     echo "3. connect to databases"
     echo "4. drop database"
     echo "5. exit"
+    echo "************"
+    echo -e "\n"
 }
 
 # Display Database menu
-database_menu(){
-
+database_menu_display(){
+echo "database_menu_display"
 }
 
 #####database_functions#####
 create_database(){
-
+read -p "Database name : " db_name
+mkdir "db/$db_name"
+echo "Database $db_name created"
 }
 
 list_databases(){
-
+echo "list databases"
 }
 
 drop_database(){
-
+echo "drop database"
 }
 
 connect_database(){
-
+echo "connect database"
 }
 ########end of database_functions######
+
+
+########database creation functions validations #####
+valid_db_name(){
+echo "valid db name"
+}
+
+db_exists(){
+echo "db exists"
+}
+######################################
+
+
 
 ####database_table_functions#####
 
 create_database_table(){
-
+echo "create table"
 }
 
 list_database_tables(){
-
+echo "list tables"
 }
 
 drop_database_table(){
-
+echo "drop table"
 }
 
 #######end of database_table_functions##########
 
 #################starting of the script################
+
+while true; do
+main_menu_display
+read -p "enter your choice : " choice
+case $choice in
+
+1) create_database ;;
+5) exit 0 ;;
+*) echo "wrong input" ;;
+
+esac
+done
